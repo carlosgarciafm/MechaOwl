43e478dd9d3cb15a3679a1c7102c86193adab1a1
Carlos García <carlosgarciafm@gmail.com>
2022-05-01 21:08:15 -0500

    feat(api): replace `discord.py` -> `disnake`
    Slash commands are already available in `disnake`. This is basically the
    reason for the change.

--- MechaOwl.py
+++ MechaOwl.py
@@ -1,11 +1,10 @@
 import os
-import discord
-from discord.ext import commands
+from disnake.ext import commands
 
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='!', intents=intents)
 token = os.environ['MECHAOWL_TOKEN']
+guild_id = os.environ['MECHAOWL_GUILDID']
+bot = commands.InteractionBot(test_guilds=[int(guild_id)])
 
 
 @bot.event
@@ -13,9 +12,10 @@ async def on_ready():
     print('Ready!')
 
 
-@bot.command()
+@bot.slash_command()
 async def hello(ctx):
-    await ctx.send(f"Hello {ctx.author.name}!")
+    """Say hello to the user."""
+    await ctx.response.send_message(f"Hello {ctx.author.name}!")
 
 
 bot.run(token)
