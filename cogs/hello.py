42e349b1c7b0d89b5538fd50c6645c3ecd7edf43
Carlos García <carlosgarciafm@gmail.com>
2022-05-05 15:06:59 -0500

    feat: use cogs to define and load commands
    I found this
    [guide](https://guide.disnake.dev/getting-started/using-cogs) a good
    introduction to cogs.

---
+++ cogs/hello.py
@@ -0,0 +1,17 @@
+import disnake
+from disnake.ext import commands
+
+
+class HelloCommand(commands.Cog):
+    """Say hello to the user."""
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.slash_command()
+    async def hello(self, inter: disnake.CommandInteraction):
+        """Say hello to the user."""
+        await inter.response.send_message(f"Hello {inter.author.name}!")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(HelloCommand(bot))
